df6efe537a2a3f2516eba92ccb612158a909b51d
Max Jänicke <max.jaenicke@gmx.de>
2020-10-31 12:48:26 +0100

    added spring project files

---
+++ src/main/java/com/finances/app/household/HouseholdApplication.java
@@ -0,0 +1,13 @@
+package com.finances.app.household;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finances/app/household/HouseholdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finances.app.household;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
