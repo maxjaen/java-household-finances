4eb4bcd5c73c8e6dbd655acc69d5fd77703d953c
Max Jänicke <max.jaenicke@gmx.de>
2020-11-09 22:14:15 +0100

    added cross origin

--- src/main/java/com/finances/app/household/controller/AccountController.java
+++ src/main/java/com/finances/app/household/controller/AccountController.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.finances.app.household.model.Account;
 import com.finances.app.household.repository.AccountRepository;
 
+@CrossOrigin
 @RestController
 public class AccountController {
 
